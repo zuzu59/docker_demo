ca6da0712112ce4708dd9de79fccb8fd4d0a5d99
Hugo Valente <hugo.valente1996@gmail.com>
2018-06-18 17:17:03 +0200

    Modifié le script run_docker.sh afin de le rendre dynamique

--- README.md
+++ README.md
@@ -2,5 +2,30 @@
 
 Création d'un container Ubuntu dans Docker 
 
+Pour installer Docker:
+
+./install.sh
+
+Pour démarrer une machine Ubuntu dans un container Docker
+
+./run_docker.sh machinename machineport
+
+Puis:
+
+./myubuntu/ubuntu_config.sh
+
+Puis: CTRL+D
+
+On se connecte après avec:
+
+ssh ubuntu@localhost -p machineport
+
+On peut lister les machines qui tournent avec:
+
+docker container ls -a
+
+
+
+
 
 
--- run_docker.sh
+++ run_docker.sh
@@ -3,8 +3,11 @@
 #zf180618.1538
 # source: https://doc.ubuntu-fr.org/docker
 
-docker container stop toto1
-docker container rm toto1
-docker run -d -p 2221:22 -v $(pwd):/myubuntu -ti --name="toto1" ubuntu
-docker exec -ti toto1 /bin/bash
+
+echo "usage: ./run_docker machinename machineport"
+
+docker container stop $1
+docker container rm $1
+docker run -d -p $2:22 -v $(pwd):/myubuntu -ti --name="$1" ubuntu
+docker exec -ti $1 /bin/bash
 
