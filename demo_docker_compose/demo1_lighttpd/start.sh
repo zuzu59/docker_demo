ef54b2f63ac449a8637af7d58bd0026b1308e72f
Christian Zufferey <christian@zufferey.com>
2020-04-23 16:24:18 +0200

    Amélioré la procédure de démarrage afin d'être résilient au reboot ;-)

--- demo_docker_compose/demo1_lighttpd/start.sh
+++ demo_docker_compose/demo1_lighttpd/start.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 #Petit script pour démarrer le binz
-#zf180924.1515
+#zf180924.1515, zf200423.1123
 # source: https://docs.docker.com/compose/reference/up
 
-docker-compose up
+docker-compose up -d
+docker-compose logs -f
+
 
 
