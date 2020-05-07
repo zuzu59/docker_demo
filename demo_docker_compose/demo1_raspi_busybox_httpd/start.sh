9d8d683db8507694188a1934ec2bbf915236fe28
Christian Zufferey <christian@zufferey.com>
2020-05-07 20:59:14 +0000

    Ajouté encore un autre serveur httpd ultra light, à seulement 1MB !

--- demo_docker_compose/demo1_raspi_busybox_httpd/start.sh
+++ demo_docker_compose/demo1_raspi_busybox_httpd/start.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 #Petit script pour démarrer le binz
-#zf180924.1515, zf200507.2258
+#zf180924.1515, zf200423.1123
 # source: https://docs.docker.com/compose/reference/up
 
 docker-compose up -d
-
-echo -e "
-
-w3m http://localhost:8088
-
-"
-
 docker-compose logs -f
 
 
